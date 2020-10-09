bf006c164269fb97d652d34b53ff99a9e25e1f76
leoalcantara <leonidio.ti@gmail.com>
2020-10-09 11:42:12 -0300

    running shot

--- js/script.js
+++ js/script.js
@@ -9,10 +9,12 @@ function start() { // Inicio da função start()
     
     //Principais variáveis do jogo
 	
-    var jogo = {}
+    var jogo = {}    
 
     var velocidade=5;
     var posicaoY = parseInt(Math.random() * 334);
+
+    var podeAtirar=true;
     
     var TECLA = {
         W: 87,
@@ -44,7 +46,7 @@ function start() { // Inicio da função start()
     movejogador();
     moveamigo();
     moveinimigo1();
-    moveinimigo2();
+    moveinimigo2();    
 	
     } // Fim da função loop()
     
@@ -82,7 +84,8 @@ function start() { // Inicio da função start()
 
         if (jogo.pressionou[TECLA.D]) {
 
-            //Chama função Disparo	
+            //Chama função Disparo
+            disparo();	
         }
 
     } // fim da função movejogador()
@@ -125,6 +128,40 @@ function start() { // Inicio da função start()
 		}
     } // Fim da função moveinimigo2()
     
-
+    function disparo() {
+	
+        if (podeAtirar==true) {
+            
+        podeAtirar=false;
+        
+        topo = parseInt($("#jogador").css("top"))
+        posicaoX= parseInt($("#jogador").css("left"))
+        tiroX = posicaoX + 190;
+        topoTiro=topo+37;
+        $("#fundoGame").append("<div id='disparo'></div");
+        $("#disparo").css("top",topoTiro);
+        $("#disparo").css("left",tiroX);
+        
+        var tempoDisparo=window.setInterval(executaDisparo, 30);
+        
+        } //Fecha podeAtirar
+     
+            function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left",posicaoX+25); 
+    
+                    if (posicaoX>900) {
+                            
+                        window.clearInterval(tempoDisparo);
+                        tempoDisparo=null;
+                        $("#disparo").remove();
+                        podeAtirar=true;
+                        
+                       }
+        } // Fecha executaDisparo()
+    } // Fecha disparo()
+    
+    
+    
 
 } // Fim da função start
